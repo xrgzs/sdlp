7408f35a33b2004c9e9f92eb161acd4643b4c65e
xrgzs <dalizxr@qq.com>
2024-05-21 18:25:23 +0800

    rand balance

--- wall/index.php
+++ wall/index.php
@@ -1,4 +1,16 @@
-<?php    
-header("Location: wetab.php");    
-exit;  
+<?php
+// 定义服务器列表
+$servers = array(
+    'wetab.php',
+    'itab.php',
+    '360cse.php'
+);
+
+// 随机选择一个服务器
+$random_index = array_rand($servers);
+$selected_server = $servers[$random_index];
+
+// 重定向到随机选择的服务器
+header("Location: " . $selected_server);
+exit;
 ?>
