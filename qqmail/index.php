82d19f6021dc12da24ba8ac920e3651d11030dcf
yukaidi <yukaidi@duck.com>
2026-05-29 19:51:23 +0800

    refactor: 重命名接口文件夹
    - pwps -> wps (金山文档)
    - qqw -> qqmail (QQ邮箱文件)
    - 更新对应的缓存前缀

--- qqmail/index.php
+++ qqmail/index.php
@@ -6,7 +6,7 @@ header('Content-Type: application/json; charset=utf-8');
 
 // 定义常量
 const DEFAULT_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0';
-const CACHE_PREFIX = 'qqw_';
+const CACHE_PREFIX = 'qqmail_';
 const CACHE_TTL = 600;
 
 // 获取请求参数
